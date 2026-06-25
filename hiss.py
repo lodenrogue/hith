2d4fe985240054aa40990a65ad99e313a791798e
lodenrogue <phyzix5761@gmail.com>
2026-06-25 23:28:41 +0800

    Added repl

---
+++ hiss.py
@@ -0,0 +1,18 @@
+from evaluate import evaluate
+
+
+def start_repl():
+    print("Welcome to Hiss. A lisp written in Python.")
+    print("Type exit to quit")
+
+    while True:
+        exp = input("> ")
+
+        if exp == "exit":
+            exit()
+        else:
+            print(evaluate(exp))
+    
+
+if __name__ == "__main__":
+    start_repl()
