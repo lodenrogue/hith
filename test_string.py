dc984ae419a525b94e34c92085b1437fb3c86ecf
lodenrogue <phyzix5761@gmail.com>
2026-07-08 10:45:55 +0800

    Added split-string function

--- test_string.py
+++ test_string.py
@@ -13,6 +13,19 @@ class TestString(unittest.TestCase):
         self.assertEqual(self.evaluate("(substring x 0 2)"), String("\"te\""))
         self.assertEqual(self.evaluate("(substring x 1 2)"), String("\"e\""))
 
+    def test_split_string_no_match(self):
+        self.evaluate("(defvar x \"test\")")
+        self.assertEqual(self.evaluate("(split-string x \"d\")"), [String("\"test\"")])
+
+    def test_split_string_one_match(self):
+        self.evaluate("(defvar x \"test\")")
+        self.assertEqual(self.evaluate("(split-string x \"e\")"), [String("\"t\""), String("\"st\"")])
+
+    def test_split_string_multiple_matches(self):
+        self.evaluate("(defvar x \"test test\")")
+        result = self.evaluate("(split-string x \"e\")")
+        self.assertEqual(self.evaluate("(split-string x \"e\")"), [String("\"t\""), String("\"st t\""), String("\"st\"")])
+
 
 if __name__ == "__main__":
     unittest.main()
